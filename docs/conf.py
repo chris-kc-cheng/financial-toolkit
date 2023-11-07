f765ecb420311ab8f6b413182ee86f30d9c2c078
chris-kc-cheng <kc1116@gmail.com>
2023-11-06 22:11:42 -0500

    Updated sphinx.yml

--- docs/conf.py
+++ docs/conf.py
@@ -6,6 +6,7 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
+import toolkit
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
--- toolkit/black_scholes.py
+++ toolkit/black_scholes.py
@@ -16,41 +16,47 @@ def d2(strike, spot, rate, time, vol, dvd):
     return d1(strike, spot, rate, time, vol, dvd) - vol * np.sqrt(time)
 
 def price_call(strike, spot, rate, time, vol, dvd):
-    """Value of an European Call Option
-    
-    .. math:: Se^{-q \\tau}\Phi(d_1) - e^{-r \\tau} K\Phi(d_2)
+    """_summary_
 
-    Args:
-        strike (_type_): Exercise price
-        spot (_type_): Spot price
-        rate (_type_): _description_
-        time (_type_): _description_
-        vol (_type_): _description_
-        dvd (_type_): _description_
+    Parameters
+    ----------
+    strike : _type_
+        _description_
+    spot : _type_
+        _description_
+    rate : _type_
+        _description_
+    time : _type_
+        _description_
+    vol : _type_
+        _description_
+    dvd : _type_
+        _description_
 
-    Returns:
-        _type_: _description_
+    Returns
+    -------
+    _type_
+        _description_
     """
+    # Se^{-q \\tau}\Phi(d_1) - e^{-r \\tau} K\Phi(d_2)
     return spot * np.exp(-dvd * time) * stats.norm.cdf(d1(strike, spot, rate, time, vol, dvd))\
            - strike * np.exp(-rate * time) * stats.norm.cdf(d2(strike, spot, rate, time, vol, dvd))
 
 def price_put(strike, spot, rate, time, vol, dvd):
-    """Value of an European Put Option
-
-    .. math::
-        e^{-r \\tau} K\Phi(-d_2) -  Se^{-q \\tau}\Phi(-d_1)
+    """_summary_
 
     Args:
-        strike (_type_): _description_
-        spot (_type_): _description_
-        rate (_type_): _description_
-        time (_type_): _description_
-        vol (_type_): _description_
-        dvd (_type_): _description_
+        strike (float): _description_
+        spot (float): _description_
+        rate (float): _description_
+        time (float): _description_
+        vol (float): _description_
+        dvd (float): _description_
 
     Returns:
         _type_: _description_
     """
+    # e^{-r \\tau} K\Phi(-d_2) -  Se^{-q \\tau}\Phi(-d_1)    
     return strike * np.exp(-rate * time) * stats.norm.cdf(-d2(strike, spot, rate, time, vol, dvd))\
         - spot * np.exp(-dvd * time) * stats.norm.cdf(-d1(strike, spot, rate, time, vol, dvd))
 
