afd8d3648b6cd31da2ee69c073940b9473645a26
chris-kc-cheng <kc1116@gmail.com>
2025-01-03 20:32:12 -0500

    Fixed headers

--- toolkit/data.py
+++ toolkit/data.py
@@ -333,8 +333,7 @@ def get_spglobal_bulk(codes: list = [5457755]) -> pd.DataFrame:
     """
     url = f"https://www.spglobal.com/spdji/en/util/redesign/get-index-comparison-data.dot?compareArray={'&compareArray='.join([str(i) for i in codes])}&periodFlag=tenYearFlag&language_id=1"
     headers = {
-        "User-Agent": "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.6367.82 Mobile Safari/537.36",
-        "referer": "https://www.spglobal.com/spdji/en/indices/equity/sp-tsx-composite-index/",
+        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36 Edg/131.0.0.0",
         "x-requested-with": "XMLHttpRequest"
     }
     json = requests.get(url, headers=headers).json()
